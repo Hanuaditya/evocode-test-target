ceaba5653bdd9e6e47a5431772962401aa3e7368
Hanuaditya <hanu.aditya1@gmail.com>
2026-04-25 21:55:20 +0530

    EvoCode: fix issue #12 — app/components/About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -46,6 +46,25 @@ const About = ({isDarkMode}) => {
                                 <p className='text-gray-600 text-sm dark:text-white/80'> {description}</p>
                             </li>
                         ))}
+                            <li
+                                className='
+                                 border-[0.5px]
+                                 border-gray-400 
+                                 rounded-xl 
+                                 p-6 
+                                 cursor-pointer
+                                 hover:bg-lightHover 
+                                 hover:-translate-y-1
+                                 transition
+                                 duration-300
+                                 hover:shadow-black
+                                 dark:border-white 
+                                 dark:hover:shadow-white
+                              dark:hover:bg-darkHover/50
+                                  '>
+                                <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>Achievements</h3>
+                                <p className='text-gray-600 text-sm dark:text-white/80'>Hackfest-26 2nd runner up 36 hr national level hackathon with 30000 cash prize.</p>
+                            </li>
                     </ul>
 
 
